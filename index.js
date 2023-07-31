c8c00e6046a8ad48bab2b1a5d4ea6b18b196179c
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-07-31 15:53:59 +0200

    Yes

---
+++ index.js
@@ -0,0 +1,5 @@
+const person1 = require("./person.js");
+// const numb1 = person1("Eighton-Lee", "Paulse", "eighton@gmail.com");
+console.log(new person1("Eighton-Lee", "Paulse", "eighton@gmail.com"));
+console.log(new person1("Laiquah", "Cotie", "Laqua@gmail.com"));
+console.log(new person1("Ethan", "Oliver", "Ollieboi@gmail.com"));
